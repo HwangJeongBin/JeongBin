fb91f26623a0a6587dcbfc7b806380d6e2ad849a
HwangJeongBin <austinjb@naver.com>
2024-03-25 19:57:01 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,25 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        int fatMan;
-        ArrayList arrayList = new ArrayList();
+        float fatMan;
+        int fatManIndex;
+        ArrayList <Student>arrayList = new ArrayList<Student>();
         arrayList.add(new Student("홍길동",175,75));
         arrayList.add(new Student("이서경", 180, 78));
         arrayList.add(new Student("이영희", 155, 49));
-        arrayList.add(new Student("김멋사", 182, 82));
-        fatMan = arrayList.
+        arrayList.add(new Student("김멋사123", 182, 82));
+        fatMan = arrayList.get(0).getBMI();
+        fatManIndex = 0;
+        for(int i=1;i<arrayList.size();i++) {
+            if(fatMan<arrayList.get(i).getBMI()) {
+                fatMan = arrayList.get(i).getBMI();
+                fatManIndex=i;
+            }
+        }
+        arrayList.remove(fatManIndex);
+        for(int i=0;i<arrayList.size();i++) {
+            arrayList.get(i).printIntro();
+            arrayList.get(i).say();
+        }
     }
 }
